3f7edc5f16b69307dde8dc55ff4b89923022648e
DavidH404 <133894052+DavidH404@users.noreply.github.com>
2023-06-01 14:54:31 -0400

    Update DeleteContacts.php to delete by ID

--- LAMPAPI/DeleteContacts.php
+++ LAMPAPI/DeleteContacts.php
@@ -5,6 +5,7 @@
 	$firstName = $inData["firstName"];
 	$lastName = $inData["lastName"];
 	$userId = $inData["userId"];
+	$id = $inData["id"];
 	
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error)
@@ -13,8 +14,8 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName = ? AND LastName = ? AND UserID = ?");
-		$stmt->bind_param("ssi", $firstName, $lastName, $userId);
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName = ? AND LastName = ? AND UserID = ? AND ID = ?");
+		$stmt->bind_param("ssii", $firstName, $lastName, $userId, $id);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
